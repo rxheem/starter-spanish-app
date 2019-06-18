cd0d4dad94a79d10a37e577c04578e15e3791990
Raheem McDonald <raheemmcdonald@Raheems-MacBook-Pro.local>
2019-06-18 00:47:30 -0500

    Initial commit :)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'normalize.css/normalize.css';
+import 'animate.css';
+import 'bulma/css/bulma.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Bootstrap essentials
+import 'bootstrap/dist/css/bootstrap-grid.css';
+
+// Imports
+import { BrowserRouter as Router } from 'react-router-dom';
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
 serviceWorker.unregister();
---
+++ src/views/Navbar.js
@@ -0,0 +1,86 @@
+import React, { Fragment } from 'react';
+
+const Navbar = props => {
+  return (
+    <Fragment>
+      <nav className='navbar' role='navigation' aria-label='main navigation'>
+        <div className='navbar-brand'>
+          <a className='navbar-item' href='/'>
+            <img
+              src='https://bulma.io/images/bulma-logo.png'
+              width='112'
+              height='28'
+            />
+          </a>
+
+          <a
+            role='button'
+            className='navbar-burger burger'
+            aria-label='menu'
+            aria-expanded='false'
+            data-target='navbarBasicExample'
+          >
+            <span aria-hidden='true' />
+            <span aria-hidden='true' />
+            <span aria-hidden='true' />
+          </a>
+        </div>
+
+        <div id='navbarBasicExample' class='navbar-menu'>
+          <div className='navbar-start'>
+            <a className='navbar-item' href='/aim/'>
+              Aim
+            </a>
+            <a className='navbar-item' href='/overview/'>
+              Overview
+            </a>
+            <a className='navbar-item' href='/contact/'>
+              Contact
+            </a>
+            <div className='navbar-item has-dropdown is-hoverable'>
+              <a className='navbar-link'>More</a>
+
+              <div className='navbar-dropdown'>
+                <a className='navbar-item' href='/why-learn-another-language/'>
+                  <i
+                    className='fas fa-globe-europe'
+                    style={{ paddingRight: '7px', color: 'blue' }}
+                  />
+                  Why Learn Another Language
+                </a>
+                <a className='navbar-item' href='/download-beginners-guide/'>
+                  <i
+                    className='far fa-file-pdf'
+                    style={{ paddingRight: '11.5px', color: 'red' }}
+                  />
+                  Download Our Beginner's Guide
+                </a>
+                <hr className='navbar-divider' />
+                <a className='navbar-item' href='/report-an-issue/'>
+                  <i
+                    className='fas fa-bug'
+                    style={{ paddingRight: '7px', color: 'brown' }}
+                  />
+                  Report an issue
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className='navbar-end'>
+            <div className='navbar-item'>
+              <div className='buttons'>
+                <a className='button is-primary'>
+                  <strong>Sign up</strong>
+                </a>
+                <a className='button is-light'>Log in</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </Fragment>
+  );
+};
+
+export default Navbar;
